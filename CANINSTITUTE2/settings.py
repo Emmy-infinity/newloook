0594eea0575e56017806d38e55e482efcad13d1a
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2023-11-12 14:31:06 +0300

    Update settings.py

--- CANINSTITUTE2/settings.py
+++ CANINSTITUTE2/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ugdaslhgwifdliouchlewrdoche'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -99,10 +99,7 @@ prod_db=dj_database_url.config(conn_max_age=5000)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.config()
 }
 
 
