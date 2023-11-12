f6a76745a9f7924fd438b8b801b338d45a57309a
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2023-11-12 21:33:55 +0300

    Update settings.py

--- CANINSTITUTE2/settings.py
+++ CANINSTITUTE2/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS =(os.path.join(BASE_DIR, '/static'),)
-django_heroku.settings(locals())
+
 WHITENOISE_USE_FINDERS = True
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -150,3 +150,4 @@ NEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
