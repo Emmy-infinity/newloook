64fffe6b6d43c088e6271bb6092ebbb478c1ec3b
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2023-11-13 07:52:57 +0300

    Update views.py

--- elearningplatform/views.py
+++ elearningplatform/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect,render
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,PageNotAnInteger
 from django.contrib.auth import login, logout,authenticate
 from .forms import *
 
